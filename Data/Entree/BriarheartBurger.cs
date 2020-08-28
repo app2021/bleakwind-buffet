fcce9849564d94a368f3e25bbdca0f463e5d8687
Nick <Nick@DESKTOP-CFF9TFM>
2020-08-27 20:00:45 -0500

    backup

---
+++ Data/Entree/BriarheartBurger.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Data.Entree
+{
+    public class BriarheartBurger
+    {
+
+        public double Price => 6.32;
+
+        public uint Calories => 732;
+
+        private bool ketchup;
+        public bool Ketchup
+        {
+            get
+            {
+                return ketchup;
+            }
+            set
+            {
+                if (!value) specialInstructions.Add("Hold ketchup");
+                else specialInstructions.Remove("Hold ketchup");
+                ketchup = value;
+            }
+        }
+
+        public bool Bun { get; set; } = true;
+
+        private bool mustartd = true;
+        
+        public bool Mustard
+        {
+            get => Mustard;
+            set => Mustard = value;
+        }
+
+
+        private List<string> specialInstructions = new List<string>();
+        public List<string> SpecialInstructions
+        {
+            get => new List<string>(specialInstructions);
+        }
+
+    }
+}
